9dd2e30a663ef6cf34a8cf08d69aeddf7be36d5e
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-03-28 22:01:57 +0500

    removed bootstrap files in index and put them into app file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import './_app.scss';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
